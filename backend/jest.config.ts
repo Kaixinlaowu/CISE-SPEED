76d35eec679fb7440238140b63fc2072b5fa9697
2247582662@qq.com <19357399908@163.com>
2025-11-15 11:50:20 +0800

    test

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,5 +1,15 @@
-module.exports = {
+// backend/jest.config.ts
+import type { Config } from 'jest';
+
+const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/src/**/*.(spec|test).ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.(spec|test)\\.ts$',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
+
+export default config;
